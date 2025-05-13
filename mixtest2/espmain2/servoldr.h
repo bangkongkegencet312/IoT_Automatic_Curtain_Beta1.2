72207827a84a6da898603e621dfe31cf278dbd36
bangkongkegencet312 <149356905+bangkongkegencet312@users.noreply.github.com>
2025-05-13 19:23:07 +0700

    servoldr.h

--- mixtest2/espmain2/servoldr.h
+++ mixtest2/espmain2/servoldr.h
@@ -1,21 +1,15 @@
-#include "servoldr.h"
+#ifndef SERVO_CONTROL_H
+#define SERVO_CONTROL_H
+
+#include <ESP32Servo.h>
+#include <math.h>
 #include <Arduino.h>
 
-Servo myServo;
+#define SERVO_PIN 18
 
-void setupServo() {
-  myServo.attach(SERVO_PIN);
-}
+extern Servo myServo;
 
-void kontrolServo(float suhu, int intensitas) {
-  if (suhu < 30.0 && intensitas < 50) {
-    myServo.write(0);
-    Serial.println("Servo ke 0 derajat (suhu < 30 dan intensitas < 50%)");
-  } else if (suhu >= 30.0 && intensitas >= 50) {
-    myServo.write(90);
-    Serial.println("Servo ke 90 derajat (suhu >= 30 dan intensitas >= 50%)");
-  } else {
-    myServo.write(45);
-    Serial.println("Servo ke 45 derajat (kondisi campuran)");
-  }
-}
+void setupServo();
+void kontrolServo(float suhu, int intensitas);
+
+#endif
